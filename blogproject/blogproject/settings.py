a000e09963313b330f562a03a4af2a81bf32ec85
getitfrommike <getitfrommike@gmail.com>
2025-07-26 00:46:47 +0000

    Updated ALLOWED_HOSTS, admin.css, styles, and CKEditor static files

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'your-secret-key-here'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'blog.thugcoding.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
